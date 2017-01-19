02c6243ab7311e2bd1a08ffeabfd27b6dbbb8faf
Alex Jegtnes <alex@jegtnes.com>
2017-01-19 16:50:47 +0000

    Add some arcane magic to improve autocompletion?

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -5,6 +5,16 @@ fpath=($ZSH/functions $fpath)
 
 autoload -U $ZSH/functions/*(:t)
 
+# start arcane magic
+# https://coderwall.com/p/jpj_6q/zsh-better-history-searching-with-arrow-keys
+autoload -U up-line-or-beginning-search
+autoload -U down-line-or-beginning-search
+zle -N up-line-or-beginning-search
+zle -N down-line-or-beginning-search
+bindkey "^[[A" up-line-or-beginning-search # Up
+bindkey "^[[B" down-line-or-beginning-search # Down
+# end arcane magic
+
 HISTFILE=~/.zsh_history
 HISTSIZE=50000
 SAVEHIST=50000
