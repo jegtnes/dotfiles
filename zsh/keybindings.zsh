71e795a93719593a3572da972a63a7ec9762ce74
jegtnes <alex@jegtnes.com>
2023-07-19 13:05:26 +0100

    Add better keybindings

---
+++ zsh/keybindings.zsh
@@ -0,0 +1,14 @@
+
+bindkey -s '^[3' \#
+
+# fix zsh annoying history behavior
+h() { if [ -z "$*" ]; then history 1; else history 1 | egrep "$@"; fi; }
+
+autoload -Uz up-line-or-beginning-search
+autoload -Uz down-line-or-beginning-search
+zle -N up-line-or-beginning-search
+zle -N down-line-or-beginning-search
+bindkey '\eOA' up-line-or-beginning-search
+bindkey '\e[A' up-line-or-beginning-search
+bindkey '\eOB' down-line-or-beginning-search
+bindkey '\e[B' down-line-or-beginning-search
