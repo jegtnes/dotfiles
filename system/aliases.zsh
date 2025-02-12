75cfe273402ee2009ef8674d7a0d24a362db37f7
Alex Jegtnes <alex@jegtnes.com>
2025-02-12 15:52:21 +0000

    Actually use eza (exa replacement)

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,16 +1,5 @@
-# grc overides for ls
-#   Made possible through contributions from generous benefactors like
-#   `brew install coreutils`
-if $(gls &>/dev/null)
+if $(eza &>/dev/null)
 then
-  alias ls="gls -F --color"
-  alias l="gls -lAh --color"
-  alias ll="gls -l --color"
-  alias la='gls -A --color'
-fi
-
-if $(exa &>/dev/null)
-then
-  alias l='exa -F'
-  alias ll='exa -Fgla'
+  alias l='eza -F'
+  alias ls='eza'
 fi
